f2cf94ce034d3bcad23bc39ee3018405f24b50d7
JUMMAN SK <92851033+akashsk8@users.noreply.github.com>
2025-06-14 10:54:09 +0530

    PortFolio Files

---
+++ Home.js
@@ -0,0 +1,63 @@
+let MainCont = document.getElementById('MCont')
+let Cont2 = document.getElementById('Cont2')
+let Dp2Photo = document.getElementById('Dp2Photo')
+let SkillTag = document.getElementById('SkillTag')
+let skill1 = document.getElementById('skill1')
+let skill2 = document.getElementById('skill2')
+let skill3 = document.getElementById('skill3')
+let skill4 = document.getElementById('skill4')
+let skill5 = document.getElementById('skill5')
+let topBar = document.getElementById('topBar')
+let midBar = document.getElementById('midBar')
+let lowerBar = document.getElementById('lowerBar')
+setTimeout(()=>{
+    MainCont.style.width = '130%'
+},1000)
+// setTimeout(() => {
+//     Cont2.style.marginLeft = '15rem'
+// }, 2000);
+setTimeout(() => {
+    Dp2Photo.style.transform = 'translateX(0%)'
+},2800);
+setTimeout(() => {
+    SkillTag.style.transform ='translateX(0%)'
+},3800);
+setTimeout(() => {
+    skill1.style.transform = 'translateX(-22.3rem)'
+},4800);
+setTimeout(() => {
+    skill2.style.transform = 'translateX(-22.3rem)'
+},5800);
+setTimeout(() => {
+    skill3.style.transform = 'translateX(-22.3rem)'
+},6800);
+setTimeout(() => {
+    skill4.style.transform = 'translateX(-22.3rem)'
+},7800);
+setTimeout(() => {
+    skill5.style.transform = 'translateX(-22.3rem)'
+},8800);
+// let con = false;
+// let toggle = ()=>{
+//     con = !con
+//     if(con===true){
+//     midBar.style.opacity = '0'
+//     topBar.style.left = '79.4%'
+//     topBar.style.top = '1.9rem'
+//     topBar.style.background = 'red'
+//     topBar.style.transform = 'rotate(45deg)'
+//     lowerBar.style.left = '79.4%'
+//     lowerBar.style.top = '1.9rem'
+//     lowerBar.style.background = 'red'
+//     lowerBar.style.transform = 'rotate(-45deg)'
+//     }
+//     else{
+//     midBar.style.opacity = '100'
+//     topBar.style.left = '0'
+//     topBar.style.bottom = '65.2rem'
+//     topBar.style.transform = 'rotate(0deg)'
+//     lowerBar.style.left = '0'
+//     lowerBar.style.top = '2.5rem'
+//     lowerBar.style.transform = 'rotate(0deg)'
+//     }
+// }
